c9a41d63ac8306f1d56bac58cea67ebeb180e6c5
qq96932100 <qq9693210000@gmail.com>
2022-06-12 15:33:02 +0800

    NEW/JS跳轉功能
    新增跳轉功能

--- django/sample/sample/static/js/btn.js
+++ django/sample/sample/static/js/btn.js
@@ -4,10 +4,15 @@ var search_bar_btn = document.getElementById('search_bar_btn');
 search_bar_btn.addEventListener('click', function () {
     console.log('Parent Capturing');
     show_stock_number();
+    new_page();
   }, true);
 
   
 function show_stock_number() {
-    let search_bar_input = document.getElementById("search_bar_input")
-    document.getElementById("stock_number").innerHTML = search_bar_input.value;
-}
+      let search_bar_input = document.getElementById("search_bar_input")
+      document.getElementById("stock_number").innerHTML = search_bar_input.value;
+  }
+
+function new_page() {
+    location.replace("http://127.0.0.1:5500/django/sample/templates/index.html")
+  }

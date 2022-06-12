f71b63f8cdf7365f36f9b69916f3bfb6ecb1829e
sido52245224 <sido52245224@gmail.com>
2022-06-12 20:42:24 +0800

    back_end資料夾新增
    新增了10張股票的一整年資訊

---
+++ django/sample/back_end/stock.py
@@ -0,0 +1,36 @@
+import twstock
+import pandas as pd
+
+def csv_make2021(target_price,i,j):
+    df = pd.DataFrame(columns=name_attribute, data=target_price)
+    filename = f'C:/Users/xido5/Desktop/back_end/'+all[j]+' 2021 '+str(i)+'月.csv'
+    df.to_csv(filename)
+    
+def csv_make2022(target_price,i,j):
+    df = pd.DataFrame(columns=name_attribute, data=target_price)
+    filename = f'C:/Users/xido5/Desktop/back_end/'+all[j]+' 2022 '+str(i)+'月.csv'
+    df.to_csv(filename)
+
+def all_stock(target,j):
+    for i in range(6,13):
+        target_price = target.fetch(2021, i)
+        csv_make2021(target_price,i,j)
+        print(i)
+    for i in range(1,7):
+        target_price = target.fetch(2022, i)
+        csv_make2022(target_price,i,j)
+        print(i)
+
+all=["0050","2330","2884","1101","1102","2002","2412","2823","5880","2892"]
+
+name_attribute = [
+        'Date', 'Capacity', 'Turnover', 'Open', 'High', 'Low', 'Close', 'Change',
+        'Transcation'
+        ]
+
+for j in range(2,10):
+    target = twstock.Stock(all[j])
+    all_stock(target,j)
+
+
+

7c676facb156cbee2589b0b0ac166f7dc36839b7
qq96932100 <qq9693210000@gmail.com>
2022-06-12 19:13:35 +0800

    FIX/修正路徑
    修正runserver無法讀取static的問題

--- django/sample/sample/settings.py
+++ django/sample/sample/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sampleApp'
+    'sampleApp',
 ]
 
 MIDDLEWARE = [
@@ -120,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STRTICFLIES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_URL = '/static/'
+HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.join(HERE, '../')
+STATICFILES_DIRS = (
+    os.path.join(HERE, 'static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

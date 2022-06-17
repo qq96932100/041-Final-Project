9d465e5f1088ca6ab40b6303d2b67f79959493c6
Justinchien <123xxjustinxx123@gmail.com>
2022-06-17 12:28:28 +0800

    FIX / 改正網頁顯示
    修正 選單與按鈕同時消失
    註: info about_us 按鈕仍然無效

--- django/sample/static/js/btn.js
+++ django/sample/static/js/btn.js
@@ -7,11 +7,11 @@ search_bar_btn.addEventListener('click', function () {
     if(get_input() != ''){
       set_stock_number();
       send_data();
-      console.log('par:' + url.search());    
+      console.log('par:' + url.search());
     }
     // reload();
   }, true);
- 
+
 function get_input() {
       let search_bar_input = document.getElementById("search_bar_input").value;
       return search_bar_input;
@@ -29,14 +29,14 @@ function send_data() {
     var xhr;
     if (window.XMLHttpRequest) { //檢查瀏覽器的XMLHttpRequest屬性，如果為真則支持XMLHttpRequest
       // IE7+, Firefox, Chrome, Opera, Safari 瀏覽器執行代碼
-      xhr=new XMLHttpRequest(); 
+      xhr=new XMLHttpRequest();
     } else {
       // IE6, IE5 瀏覽器執行代碼
-      xhr=new ActiveXObject("Microsoft.XMLHTTP"); 
+      xhr=new ActiveXObject("Microsoft.XMLHTTP");
     }
- 
+
   /*
-    xhr.open(get,url,async);  
+    xhr.open(get,url,async);
     send(string);  //post請求時才使用字符串參數，否則不用帶參數。
 
     method：請求的類型，常見的get或post方法
@@ -53,7 +53,7 @@ function send_data() {
   // xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=utf-8"); //必須寫在open和send中間
 
   // xhr.send("key=value&key2=value2");
-      
+
   /*
   注意：
   1) post請求一定要設置請求頭的格式內容;
@@ -80,10 +80,10 @@ function openNav() {
   menu = document.getElementsByClassName("menu")[0];
   menu_btn = document.getElementById('menu_button');
   // menu.style.visibility = "hidden";
-  menu.style.width = "0vw";
+  menu.style.width = "15vw";
   menu.style.opacity = 1;
   // menu_btn.style.visibility = "visible";
-  menu.style.transition = "1s";
+  menu.style.transition = "0.25s";
   // menu_btn.style.opacity = 0;
   // menu.style.visibility = "collapse";
 }
@@ -95,7 +95,7 @@ function closeNav() {
   menu.style.width = "0vw";
   menu.style.opacity = 0;
   // menu_btn.style.visibility = "visible";
-  menu.style.transition = "1s";
+  menu.style.transition = "0.25s";
   // menu_btn.style.opacity = 1;
   // menu.style.visibility = "collapse";
 }
